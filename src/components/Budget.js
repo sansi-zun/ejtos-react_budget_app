3eb63627139d99350886c2798ab5099693c08efe
Some Name <ralf.holger@keemail.me>
2023-03-05 12:49:54 -0500

    in progress

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,23 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { currency, budget, dispatch } = useContext(AppContext);
+    const setBudget = (e) => dispatch({ type: 'SET_BUDGET', payload: e.target.value});
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}</span>
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                value={budget}
+                style={{ marginLeft: '1px' }}
+                onChange={setBudget}>
+            </input>
         </div>
     );
-};
+}; 
+
 export default Budget;
