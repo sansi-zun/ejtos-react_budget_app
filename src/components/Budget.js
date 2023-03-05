7c18b87b1696e60eda6f516d30ebf2dd9bba5463
Some Name <ralf.holger@keemail.me>
2023-03-05 16:54:00 -0500

    added missing feature in budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,8 +2,24 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { currency, budget, dispatch } = useContext(AppContext);
-    const setBudget = (e) => dispatch({ type: 'SET_BUDGET', payload: e.target.value});
+    const { currency, budget, expenses, dispatch } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const setBudget = (val) => {
+        let newBudget = val;
+        if (val > 20000) {
+            alert("The value for budget cannot exceed " + currency + "20000");
+            newBudget = 20000;
+        }
+        else if (val < totalExpenses) {
+            alert("The value for budget cannot be lower than the spending");
+            newBudget = totalExpenses;
+        }
+        dispatch({ type: 'SET_BUDGET', payload: newBudget})
+    };
 
     return (
         <div className='alert alert-secondary'>
@@ -12,9 +28,10 @@ const Budget = () => {
                 required='required'
                 type='number'
                 id='budget'
+                step="10"
                 value={budget}
                 style={{ marginLeft: '1px', maxWidth: '70%' }}
-                onChange={setBudget}>
+                onChange={(e) => setBudget(e.target.value)}>
             </input>
         </div>
     );
